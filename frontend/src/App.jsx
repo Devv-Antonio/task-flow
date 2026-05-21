4aafa881b4ced9aa039cdc55aa598bbe7806c84c
Devv-Antonio <antonio.lucasa06@gmail.com>
2026-05-21 17:15:15 -0300

    feat(frontend): configura estrutura inicial, react-router e atualiza tailwind v4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
-export default function App() {
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Tasks from './pages/Tasks'
+
+function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-5xl font-bold text-blue-500">
-        TaskFlow 🚀
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Se acessar direto o localhost, joga pro Login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        
+        {/* Nossas rotas */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/tasks" element={<Tasks />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
+
+export default App
---
+++ frontend/src/pages/Tasks.tsx
@@ -0,0 +1,7 @@
+export default function Tasks() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <h1 className="text-3xl font-bold text-gray-800">Minhas Tarefas 📋</h1>
+    </div>
+  )
+}
